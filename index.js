add68ccb8e0237a9da5b859caa21e5326579c96e
Ryan Breen <ryan@ryanbreen.com>
2021-08-08 17:34:57 +0000

    Switch to 120fps and a single ffmpeg process.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const { writeFileSync } = require('fs');
-const { spawn } = require('child_process');
+import spawn from 'await-spawn'
 
 const uri = 'rtsp://10.0.2.5:7447/4zlmq5j4Zz4PE4Hz'
 
-const grabFrame = async() => {
-  const name = Date.now()
-  this.child = spawn('ffmpeg', ['-rtsp_transport','tcp', '-i', uri,
-    '-loglevel', 'quiet', '-r', '10', '-f', 'image2', `data/${name}.png`])
-  for await (const data of this.child.stdout) {
+const run = async () => {
+  try {
+    await spawn('ffmpeg', ['-rtsp_transport','tcp', '-i', uri,
+      '-loglevel', 'quiet',  '-f', 'image2', '-pix_fmt', 'yuva444p16le', '-vf', 'fps=1/5', '-strftime', '1', `data/%s%03d.png`])
+  } catch(e) {
+    console.log(e.stderr.toString())
   }
 }
 
-grabFrame()
-setInterval(grabFrame, 10 * 1000);
+run();
+//setInterval(grabFrame, 10 * 1000);
